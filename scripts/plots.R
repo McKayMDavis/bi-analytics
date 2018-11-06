8d8d8d3f29570b2de393413756d3d63a2e4a60f9
McKay Davis <mckaymdavis@gmail.com>
2018-11-06 15:22:41 -0700

    added codes data to guns data

--- scripts/plots.R
+++ scripts/plots.R
@@ -1,13 +1,22 @@
 library(tidyverse)
-guns <- readr::read_csv("data/guns.csv")
 
-# Cleaning methods: update "hispanic" column with correct race.
 
+# Read in some data
+guns <- read_csv("data/guns.csv")
+codes <- read_csv("data/cdc_codes.csv", col_names = FALSE)
+
+# Format codes data for joining on guns data
+codes %<>%
+  mutate(code = str_extract(X1, "[0-9]+"), 
+         X1 = gsub("[0-9]+\\s", "", X1)) %>% 
+  rename(type = X1)
+
+# Join codes on guns
+guns %<>%
+  rename(code = hispanic) %>% 
+  merge(codes)
 
 # Remove NA values
 
 
 # Remove bad columns
-
-
-# 
