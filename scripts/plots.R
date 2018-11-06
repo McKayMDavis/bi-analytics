26513fc1bbd26aa2a0cf2d99f6d48a9a90cc311a
McKay Davis <mckaymdavis@gmail.com>
2018-11-06 14:48:28 -0700

    stuff

--- scripts/plots.R
+++ scripts/plots.R
@@ -1,3 +1,13 @@
 library(tidyverse)
 guns <- readr::read_csv("data/guns.csv")
 
+# Cleaning methods: update "hispanic" column with correct race.
+
+
+# Remove NA values
+
+
+# Remove bad columns
+
+
+# 
