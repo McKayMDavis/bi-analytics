171c31ed7ae52872a4364cce70cb71333633b5b7
McKay Davis <mckaymdavis@gmail.com>
2018-11-08 13:53:00 -0700

    rename file

--- scripts/script.R
+++ scripts/script.R
@@ -5,18 +5,22 @@ library(tidyverse)
 guns <- read_csv("data/guns.csv")
 codes <- read_csv("data/cdc_codes.csv", col_names = FALSE)
 
+# Cleaning method 1: McKay
 # Format codes data for joining on guns data
 codes %<>%
   mutate(code = str_extract(X1, "[0-9]+"), 
          X1 = gsub("[0-9]+\\s", "", X1)) %>% 
   rename(type = X1)
 
+# Cleaning method 2: McKay
 # Join codes on guns
 guns %<>%
   rename(code = hispanic) %>% 
   merge(codes)
 
+# Cleaning method 3: Brenden
 # Remove NA values
 
 
+# Cleaning method 4: Brenden
 # Remove bad columns
